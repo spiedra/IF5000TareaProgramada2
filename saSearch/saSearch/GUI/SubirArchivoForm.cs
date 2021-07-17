ac6f14fe29b534b3d7cd81d22cd11a940e60a8b3
Randall <randms07@hotmail.com>
2021-07-17 10:48:51 -0600

    enviando peso del archivo

--- saSearch/saSearch/GUI/SubirArchivoForm.cs
+++ saSearch/saSearch/GUI/SubirArchivoForm.cs
@@ -29,7 +29,7 @@ namespace saSearch.GUI
         private void btn_enviar_Click(object sender, EventArgs e)
         {
             Byte[] bytes = Utility.ConvertFileToByteArray(fileDirectory);
-            c.Send("archivo*"+name+"*"+tamano);
+            c.Send("archivo*"+name+"*"+tamano);//envio
             Thread.Sleep(30);
             c.sendBytesMsg(bytes);
             //
